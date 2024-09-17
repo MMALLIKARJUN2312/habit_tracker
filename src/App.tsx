57f772aec13a3c96839b4a476a36dfba903fd180
M MALLIKARJUN <77607690+MMALLIKARJUN2312@users.noreply.github.com>
2024-09-17 10:11:23 +0000

    habit_tracker

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,16 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1 className="mainHeading">Habit Tracker</h1>
+      <form className="signInForm">
+        <label htmlFor="habitName">Enter the Habit Name: </label>
+        <input type="text" id="habitName" className="userInput" required />
+        <label id="frequency">Enter the Frequency: </label>
+        <input type="text" id="frequency" className="userInput" required />
+        <button type="submit" className="addButton">
+          Add
+        </button>
+      </form>
     </div>
   );
 }
